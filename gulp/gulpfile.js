e3605d98dfbc3fa459f4d7c04019780217827a85
jiongpeng.li <lijiongpeng@yoju360.com>
2017-03-13 10:46:35 +0800

    修改

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -3,7 +3,7 @@ const gulp = require('gulp'); //本地安装gulp所用到的地方
 const jshint = require("gulp-jshint");//检查JS的代码 npm install --save-dev jshint gulp-jshint
 const less = require('gulp-less'); //编译less
 const sass = require('gulp-ruby-sass'); //编译sass 需要ruby环境并在ruby下安装sass
-const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('gulp-autoprefixer');//自动添加css3前缀
 const minify = require('gulp-minify-css');//压缩css
 const uglify = require('gulp-uglify');  //加载js压缩
 const htmlmini = require('gulp-minify-html'); //压缩html
@@ -54,7 +54,7 @@ gulp.task('sass', function () {
 
 //监听sass文件发生变化
 gulp.task('sassWatch', function () {
-    var watcher = gulp.watch(['src/sass/*.scss','src/less/*.less'], ['sass','less']);
+    var watcher = gulp.watch([paths.static.sass + '*.scss',paths.static.less + '*.less'], ['sass','less']);
     watcher.on('change',function(ev){
         console.log("flie:" + ev.path);
     })
@@ -62,78 +62,78 @@ gulp.task('sassWatch', function () {
 
 //编译css
 gulp.task('cssmini', function () {
-    gulp.src(['src/css/*.css', '!src/css/*.min.css'])
+    gulp.src([paths.static.css + '*.css', '!' + paths.static.css +'*.min.css'])
         .pipe(minify())
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest(paths.dist.css));
 });
 
 // 压缩js
 gulp.task('compass', function () {
-    gulp.src(['src/js/*.js', '!src/js/*.min.js'])
+    gulp.src([paths.static.js + '*.js', '!'+ paths.static.js +'*.min.js'])
         .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest(paths.dist.js));
 });
 
 //压缩html
 gulp.task('htmlmini', function () {
-    gulp.src('src/html/*.html')
+    gulp.src(paths.static.html + '*.html')
         .pipe(htmlmini())
-        .pipe(gulp.dest('dist/html'));
+        .pipe(gulp.dest(paths.dist.html));
 });
 
 //图片压缩
 gulp.task('imagemini', function () {
-    gulp.src('src/images/*.{png,jpg,gif,ico}')
+    gulp.src(paths.static.image + '*.{png,jpg,gif,ico}')
         .pipe(cache(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         })))
-        .pipe(gulp.dest('dist/images'));
+        .pipe(gulp.dest(paths.dist.image));
 });
 
 //js文件压缩并合并
 gulp.task('jsConcat', function () {
-    gulp.src(['src/js/*.js','!src/js/*.min.js'])
+    gulp.src([paths.static.js + '*.js','!' + paths.static.js + '*.min.js'])
         .pipe(uglify())
         .pipe(concat('all.min.js'))
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest(paths.dist.js));
 });
 
 //css文件压缩并合并
 gulp.task('cssConcat', function () {
-    gulp.src(['src/css/*.css','!src/js/*.min.css'])
+    gulp.src([paths.static.css + '*.css','!'+ paths.static.css +'*.min.css'])
         .pipe(minify())
         .pipe(concat('all.min.css'))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest(paths.dist.css));
 });
 
 //less文件压缩并合并
 gulp.task('lessConcat', function () {
-    gulp.src('src/less/*.less')
+    gulp.src(paths.static.less + '*.less')
         .pipe(less())
         .pipe(minify())
         .pipe(concat('all.min.css'))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest(paths.dist.css));
 });
 
 //sass文件压缩并合并
 gulp.task('sassConcat', function () {
-    gulp.src('src/sass/*.scss')
+    gulp.src(paths.static.sass + '*.scss')
         .pipe(less())
         .pipe(minify())
         .pipe(concat('all.min.css'))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest(paths.dist.css));
 });
 
 //自动在css3加入前缀
 gulp.task('autoCss3', function() {
-        gulp.src(['src/css/*.css','!src/css/*.min.css'])
+        gulp.src([paths.static.css + '*.css','!'+ paths.static.css +'*.min.css'])
             .pipe(autoprefixer({
                 browsers: ['last 10 versions'],
                 cascade: false
             }))
-            .pipe(gulp.dest('src/css'))
+            .pipe(gulp.dest(paths.static.css))
     }
 );
 
