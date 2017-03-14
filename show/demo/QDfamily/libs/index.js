60494d1c4ee500f64cc1908a88d00791f8a3652b
jiongpeng.li <lijiongpeng@yoju360.com>
2017-03-14 17:05:31 +0800

    修改

--- show/demo/QDfamily/libs/index.js
+++ show/demo/QDfamily/libs/index.js
@@ -118,6 +118,7 @@ var cards = $(".lottery_card");
 cards.each(function(i){
     cards.eq(i).on("click",function(){
         $.getScript('http://392417426.github.io/looder/show/demo/QDfamily/libs/json.js', function (result) {
+                var result = $.parseJSON(result);
                 var priceId = 0;
                 var src = "images/lottery_card_face04.png";
                 if(result.errcode == 200){
