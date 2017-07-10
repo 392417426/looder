25aad71f8d88a71c6c479c5e2f79887321ceae27
jiongpeng.li <lijiongpeng@yoju360.com>
2017-07-10 22:14:04 +0800

    修改

---
+++ demo/js/audio.js
@@ -0,0 +1,16 @@
+function audioAutoPlay(id){
+    var audio = document.getElementById(id),
+        play = function(){
+            audio.play();
+            document.removeEventListener("touchstart",play, false);
+        };
+    audio.play();
+    document.addEventListener("WeixinJSBridgeReady", function () {
+        play();
+    }, false);
+    document.addEventListener('YixinJSBridgeReady', function() {
+        play();
+    }, false);
+    document.addEventListener("touchstart",play, false);
+}
+audioAutoPlay('play_audio');
